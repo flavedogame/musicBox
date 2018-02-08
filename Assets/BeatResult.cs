30359b1c46fb11688e8a1c4ad8de8f20ef3b7f31
shshwdr <cs.vivian04@gmail.com>
2018-02-08 02:27:42 -0800

    add beat result

---
+++ Assets/BeatResult.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BeatResult : MonoBehaviour {
+	bool finishedSetup;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void Setup(BeatResultType result, RectTransform r){
+		RectTransform rect = GetComponent<RectTransform> ();
+		rect.localScale = new Vector3 (0.1f, 0.1f, 0.1f);
+		rect.position = r.position;
+		Text text = GetComponent<Text>();
+		switch (result) {
+		case BeatResultType.JustPerfect:
+			text.text = "嚯厉害厉害";
+			text.color = new Color (1, 1, 0);
+			text.fontSize = 60;
+			break;
+		case BeatResultType.Perfect:
+			text.text = "厉害厉害";
+			text.color = new Color (1, 0.5f, 0);
+			text.fontSize = 50;
+			break;
+		case BeatResultType.Great:
+			text.text = "真棒";
+			text.color = new Color (0, 0.2f, 1);
+			text.fontSize = 40;
+			break;
+		case BeatResultType.Good:
+			text.text = "还行";
+			text.color = new Color (0, 1, 0);
+			text.fontSize = 40;
+			break;
+		case BeatResultType.Bad:
+		case BeatResultType.Miss:
+			text.text = "哎呦喂。。";
+			text.color = new Color (1, 0, 0);
+			text.fontSize = 40;
+			break;
+
+		}
+		Destroy (gameObject, 1);
+
+
+		finishedSetup = true;
+	}
+}
--- Assets/MusicGameLogic.cs
+++ Assets/MusicGameLogic.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum BeatResultType  {JustPerfect,Perfect,Great,Good,Bad,Miss};
 public class MusicGameLogic : MonoBehaviour {
 
 	public AudioSource audioSource;
@@ -19,6 +20,7 @@ public class MusicGameLogic : MonoBehaviour {
 	public float perfectMissSeconds = 0.05f;
 	public float greatMissSeconds = 0.15f;
 	public float goodMissSeconds = 0.25f;
+	public float justPerfectSeconds = 0.03f;
 	public float perfectSeconds = 0.1f;
 	public float greatSeconds = 0.3f;
 	public float goodSeconds = 0.5f;
@@ -31,7 +33,11 @@ public class MusicGameLogic : MonoBehaviour {
 
 	public GameObject padPrefab;
 
+	public RectTransform beatRect;
+	public GameObject beatResultPrefab;
+
 	BeatmapEvent beatmap;
+
 
 	// Use this for initialization
 	void Start () {
@@ -47,6 +53,7 @@ public class MusicGameLogic : MonoBehaviour {
 			foreach (List<Beat> beatRaw in tapBeats) {
 				while (beatRaw.Count >= 1 && beatRaw [0].time + missSeconds <= timePast) {
 						beatRaw.RemoveAt (0);
+						ShowBeatResult (BeatResultType.Miss);
 						Debug.Log ("miss!");
 						i++;
 						if (i > 10) {
@@ -58,7 +65,7 @@ public class MusicGameLogic : MonoBehaviour {
 				Beat beat = beatmap.beats [0];
 				beatmap.beats.RemoveAt (0);
 				tapBeats [beat.key].Add (beat);
-				Debug.Log ("show pads");
+				//Debug.Log ("show pads");
 				ShowPads (beat);
 			}
 		}
@@ -68,6 +75,13 @@ public class MusicGameLogic : MonoBehaviour {
 		GameObject go = Instantiate (padPrefab) as GameObject;
 		go.transform.parent = transform;
 		go.GetComponent<DropingBeat>().Setup(originRect, targetRects [beat.key]);
+	}
+
+
+	void ShowBeatResult(BeatResultType result) {
+		GameObject go = Instantiate (beatResultPrefab) as GameObject;
+		go.transform.parent = transform;
+		go.GetComponent<BeatResult>().Setup(result,beatRect);
 	}
 
 	void ChangeTime(float value) {
@@ -111,19 +125,35 @@ public class MusicGameLogic : MonoBehaviour {
 			Beat beat = tapBeats [key] [0];
 			if (beat.time >= timePast + badSeconds) {
 				//ignore
+				return;
 			} else if (beat.time >= timePast + goodSeconds) {
+				ShowBeatResult (BeatResultType.Bad);
 				Debug.Log ("bad!"+beat.time+" "+timePast);
 			} else if (beat.time >= timePast + greatSeconds) {
+				ShowBeatResult (BeatResultType.Good);
 				Debug.Log ("good!"+beat.time+" "+timePast);
 			} else if (beat.time >= timePast + perfectSeconds) {
+				ShowBeatResult (BeatResultType.Great);
 				Debug.Log ("great!"+beat.time+" "+timePast);
+			} else if (beat.time >= timePast + justPerfectSeconds) {
+				ShowBeatResult (BeatResultType.Perfect);
+				Debug.Log ("perfect!"+beat.time+" "+timePast);
+			} else if (beat.time >= timePast - justPerfectSeconds) {
+				ShowBeatResult (BeatResultType.JustPerfect);
+				Debug.Log ("perfect!"+beat.time+" "+timePast);
 			} else if (beat.time >= timePast - perfectMissSeconds) {
+				ShowBeatResult (BeatResultType.Perfect);
 				Debug.Log ("perfect!"+beat.time+" "+timePast);
 			} else if (beat.time >= timePast - greatMissSeconds) {
+				ShowBeatResult (BeatResultType.Great);
 				Debug.Log ("great!"+beat.time+" "+timePast);
 			} else if (beat.time >= timePast - goodMissSeconds) {
+				ShowBeatResult (BeatResultType.Good);
 				Debug.Log ("good!"+beat.time+" "+timePast);
-			}
+			} else {
+				ShowBeatResult (BeatResultType.Bad);
+				Debug.Log ("bad!"+beat.time+" "+timePast);
+			} 
 			tapBeats [key].RemoveAt (0);
 		}
 	}
