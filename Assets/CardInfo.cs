d272b99ab49c14a37369cfe63b5b5e7aa6330806
shshwdr <cs.vivian04@gmail.com>
2018-04-06 02:19:34 -0700

    add card csv, add two images, add static card list in scene

---
+++ Assets/CardInfo.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using NUnit.Framework;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using Sinbad;
+
+public class CardInfo : MonoBehaviour {
+		public string identifier;
+		public string name;
+		public string description;
+		public string character;
+	public string normalAttack;
+	public string specialAttack;
+
+	public int deep;
+	public int pure;
+	public int passion;
+	public int funny;
+	public int think;
+
+	public Sprite image() {
+		string path = "cardImage/" + identifier;
+		Sprite sprite = Resources.Load<Sprite> (path);//format to be decided
+		//Debug.Log(sprite);
+		if (sprite == null) {
+			Debug.LogError ("sprite not exist for path: "+path);
+		}
+		return sprite;
+	}
+
+	public Sprite icon() {
+		string path = "songBG/" + identifier+"_i";
+		Sprite sprite = Resources.Load<Sprite> (path);//format to be decided
+		//Debug.Log(sprite);
+		if (sprite == null) {
+			Debug.LogError ("sprite not exist for path: "+path);
+		}
+		return sprite;
+	}
+
+//	public AudioClip audioClip(){
+//		string path = "music/" + identifier;
+//		AudioClip clip = Resources.Load<AudioClip> (path);
+//		if (clip == null) {
+//			Debug.LogError ("clip not exist for path: " + path);
+//		}
+//		return clip;
+//	}
+
+	public string Description() {
+		return "\"" + description + "\"";
+	}
+
+	public enum BasicAttribute {deep,pure,passion,funny,think};
+
+	public string basicAttribute() {
+		Dictionary<BasicAttribute,int> basicMap = new Dictionary<BasicAttribute, int> ();
+		basicMap [BasicAttribute.deep] = deep;
+		basicMap [BasicAttribute.pure] = pure;
+		basicMap [BasicAttribute.passion] = passion;
+		basicMap [BasicAttribute.funny] = funny;
+		basicMap [BasicAttribute.think] = think;
+		var newDict = basicMap.OrderByDescending(x => x.Value);
+		string result = "";
+		foreach (KeyValuePair<BasicAttribute, int> kvp in newDict) {
+			Debug.Log ("key " + kvp.Key + " value " + kvp.Value);
+			result += kvp.Key;
+		}
+		return result;
+
+	}
+
+//	static public List<SongInfo> loadSongInfo(TextAsset songInfos) {
+//		List<SongInfo> objs;
+//		using (var ms = new MemoryStream(Encoding.UTF8.GetBytes(songInfos.text))) {
+//			using (var sr = new StreamReader(ms,System.Text.Encoding.UTF8)) {
+//				objs = CsvUtil.LoadObjects<SongInfo>(sr);
+//			}
+//		}
+//		return objs;
+//	}
+//
+//	static public List<SongInfo> loadSongInfo(string songInfosName) {
+//		return CsvUtil.LoadObjects<SongInfo> (songInfosName);
+//	}
+}
